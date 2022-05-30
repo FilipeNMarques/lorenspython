961ffdd9330f6b8a58c4d7ee55bb794ce2122502
Filipe Marques <filipi.marques@gmail.com>
2022-05-30 17:58:30 -0300

    add first task

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Lidar com arquivos estáticos no projeto, ex: .txt
+# Utilizar chamadas à API externa
+# Serializar dados (Pesquisar o que é)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Buscador de ativos cripto
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Utilizar a api do mercado bitcoin
+
+# ex de chamada: https://www.mercadobitcoin.net/api/BTC/ticker/
+
+# Doc da api: https://www.mercadobitcoin.com.br/api-doc/#metodos_api_dado
+
+# ex de retorno passando BTC na url
+
+# {
+#   "ticker": {
+#     "high": "147253.00000000",
+#     "low": "137000.00000000",
+#     "vol": "74.05813683",
+#     "last": "146951.66443454",
+#     "buy": "146961.0544497",
+#     "sell": "147100",
+#     "open": "139037.96630000",
+#     "date": 1653942192
+#   }
+# }
+
+# Baseado na input do usuário, retornar para ele o maior, menor e o preço atual do ativo que foi digitado
+
+#  libs para fazer o sistema: requests
+
+
+# TODO: fazer a primeira chamada na api utilizando a url de exemplo e printando o retorno, qualquer moeda disponível na api
+
+
+# 1. Utilizar uma função para bater na api, dentro dessa func, chamar o requests e printar o resultado
+
+
